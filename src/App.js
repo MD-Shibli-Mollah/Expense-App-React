c4b200943d2b9e9b5f5204a9c3a60c767c423c76
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-06-02 12:26:28 +0600

    useState to update the state

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import Expenses from "./components/Expenses/Expenses";
 
+// function App(){return...}
 const App = () => {
   const expenses = [
     { title: "Electricity Bill", amount: 500, date: new Date(2022, 6, 1) },
