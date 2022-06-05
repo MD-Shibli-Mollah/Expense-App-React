576d430db8ba88f2c8f684dc7227c513930b4208
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-06-05 17:33:03 +0600

    Test Button removed & functions commented ..

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+//import React, { useState } from 'react';
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 import "./ExpenseItem.css";
@@ -6,21 +6,20 @@ import "./ExpenseItem.css";
 
 const ExpenseItem = (props) => {
 
-  const [title, setTitle]= useState(props.title); //REACT HOOK//
+  // const [title, setTitle]= useState(props.title); //REACT HOOK//
   //console.log([title, setTitle]);
-  const clickHandler = () => {
+  //const clickHandler = () => {
     //title = "Updated";
-    setTitle("Updated");
+  //  setTitle("Updated");
    // console.log("title");
-  };
+  //};
   return (
     <Card className="expense-item">
       <ExpenseDate date={props.date} />
       <div className="expense-item__description">
-        <h2>{title}</h2>
+        <h2>{props.title}</h2>
         <div className="expense-item__price">{props.amount}</div>
       </div>
-      <button onClick={clickHandler}>Click Here</button>
     </Card>
   );
 }
