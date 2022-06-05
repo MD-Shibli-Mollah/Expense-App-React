dedd53a5ef60fe1579066193c908e2d549c39713
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-06-05 23:19:29 +0600

    Using props.items.map() for array handling

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -16,7 +16,8 @@ const Expenses = (props) => {
     <div>
       <Card className='expenses'>
         <ExpensesFilter selected={filteredYear} onChangeFilter={filterChangeHandler} />
-        <ExpenseItem
+        {props.items.map(expense => <ExpenseItem title = {expense.title} amount = {expense.amount} date = {expense.date}/>)}
+        {/* <ExpenseItem
           title={props.items[0].title}
           amount={props.items[0].amount}
           date={props.items[0].date}
@@ -35,7 +36,7 @@ const Expenses = (props) => {
           title={props.items[3].title}
           amount={props.items[3].amount}
           date={props.items[3].date}
-        />
+        /> */}
       </Card>
     </div>
   );
