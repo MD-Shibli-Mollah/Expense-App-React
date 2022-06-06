1c32b8cb6eb23e2fe25811e39867d2711468a8f9
MD-Shibli-Mollah <md.ta.shibli@gmail.com>
2022-06-06 16:40:36 +0600

    Unique id for Key

--- src/App.js
+++ src/App.js
@@ -1,17 +1,25 @@
+import React, {useState} from 'react';
 import Expenses from "./components/Expenses/Expenses";
 import NewExpense from "./components/NewExpense/NewExpense";
+
 // function App(){return...}
+
+const DUMMY_EXPENSES = [
+  { id: "e1", title: "miscellaneous", amount: 500, date: new Date(2022, 6, 1) },
+  { id: "e2", title: "House Rent", amount: 409.05, date: new Date(2022, 6, 3) },
+  { id: "e3", title: "Gas Bill", amount: 201, date: new Date(2022, 6, 5) },
+  { id: "e4", title: "Medicines", amount: 453, date: new Date(2022, 6, 9) },
+];
+
 const App = () => {
-  const expenses = [
-    { title: "Electricity Bill", amount: 500, date: new Date(2022, 6, 1) },
-    { title: "House Rent", amount: 409.05, date: new Date(2022, 6, 3) },
-    { title: "Gas Bill", amount: 201, date: new Date(2022, 6, 5) },
-    { title: "Medicines", amount: 453, date: new Date(2022, 6, 9) },
-  ];
+const [expenses, setExpenses] = useState(DUMMY_EXPENSES);
 
   const addExpenseHandler = expense => {
-    console.log("In App.js");
-    console.log(expense);
+      setExpenses(prevExpenses => {
+        return [expense, ...prevExpenses];
+      });   
+    //console.log("In App.js");
+    //console.log(expense);
   };
 
   return (
